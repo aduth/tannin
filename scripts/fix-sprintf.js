2fa9e1afa476fc7a0163065759026436626cd991
USERSATOSHI <tushar.bharti@rtcamp.com>
2025-06-13 21:55:05 +0530

    feat: add fix-sprintf script to update index.d.ts in sprintf package

---
+++ scripts/fix-sprintf.js
@@ -0,0 +1,14 @@
+import { readFile, writeFile } from 'fs/promises';
+
+
+const content = await readFile('./packages/sprintf/src/index.d.ts', 'utf8'),
+	updatedContent = content.replace(
+		'...args: import("../types").SprintfArgs<T>[]',
+		'...args: import("../types").SprintfArgs<T>',
+	);
+
+await writeFile('./packages/sprintf/src/index.d.ts', updatedContent, 'utf8');
+
+// eslint-disable-next-line no-console
+console.log('Updated index.d.ts in sprintf package');
+
